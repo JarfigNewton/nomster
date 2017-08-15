a4b93f3e1292a2736fed6289f1048f8086cb0d7f
JarfigNewton <jaredschil@gmail.com>
2017-08-15 20:49:08 +0000

    Added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates :name, presence: true, length: { minimum: 4, message: "Must be at least 4 characters" }
   validates :address, presence: true
   validates :description, presence: true
